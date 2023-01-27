84123c065c6902de45f9d30853891c66847eda0d
Erderis <56418185+erderis@users.noreply.github.com>
2023-01-27 09:36:39 +0700

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -44,7 +44,7 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCWGQIULo_4YABY9pMRU5IvLy1T5-qSEgA',
+    apiKey: '',
     appId: '1:663337299852:web:b69be2f400fa61b049f0a9',
     messagingSenderId: '663337299852',
     projectId: 'erderis',
@@ -53,7 +53,7 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCWGQIULo_4YABY9pMRU5IvLy1T5-qSEgA',
+    apiKey: '',
     appId: '1:663337299852:android:3b5e8eccc7289a9c49f0a9',
     messagingSenderId: '663337299852',
     projectId: 'erderis',
@@ -72,7 +72,7 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyCWGQIULo_4YABY9pMRU5IvLy1T5-qSEgA',
+    apiKey: '',
     appId: '1:663337299852:ios:80ffb02e2e1cc1e249f0a9',
     messagingSenderId: '663337299852',
     projectId: 'erderis',
